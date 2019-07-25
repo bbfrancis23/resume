9abc3fab847eb01a9b6e81208df9a1db04fd9644
Brian <bbfrancis23@gmail.com>
2019-07-25 09:31:11 -0700

    web strom changes

--- src/app/page/page-items/resume-intro/resume-intro.component.ts
+++ src/app/page/page-items/resume-intro/resume-intro.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { Unsubscriber } from '../../../unsubscriber/unsubscriber'
-import { }
+
 
 
 export interface PhoneNumber {
@@ -21,6 +21,7 @@ export interface ResumeIntro {
 }
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'resume-intro',
   templateUrl: 'resume-intro.component.html'
 })
@@ -33,11 +34,13 @@ export class ResumeIntroComponent extends Unsubscriber {
     email: 'bbfrancis23@gmail.com',
     phone: { countryCode: 1, areaCode: 801, prefix: 867, lineNumber: 1885 },
     phoneString: '',
-    text: 'I have had over 15 year experience in Web Development and Programming. I studied Computer Science at Weber State University. I was responsible for the “Marine Classifieds” program at iboats.com. This program collects, edits, displays marine advertisements (mainly boats for sale) on boats.iboats.com, from boat dealers, brokers and individual users. This included “For Sale By Owner”, “New Boat research”, “Dealer Locator” and various backend administration programs. During my tenure at iboats classifieds ads went from a few hundred to over fifty thousand. I have worked various freelance job. I worked at Arizona Department of Environmental Quality at first as a Contractor and then was hired as an Angular Specialist.'
+    // tslint:disable-next-line:max-line-length
+    text: 'I have had over 15 year experience in Web Development and Programming. I studied Computer Science at Weber State University. I was responsible for the “Marine Classifieds” program at iboats.com. This program collects, edits, displays marine advertisements (mainly boats for sale) on boats.iboats.com, from boat dealers, brokers and individual users. This included “For Sale By Owner”, “New Boat research”, “Dealer Locator” and various backend administration programs. During my tenure at iboats classifieds ads went from a few thousand to nearly fifty thousand. I have worked various freelance job. I worked at Arizona Department of Environmental Quality at first as a Contractor and then was hired as an Angular Specialist.'
   }
 
   constructor() {
     super()
-    this.resumeIntro.phoneString = `${this.resumeIntro.phone.countryCode}-${this.resumeIntro.phone.areaCode}-${this.resumeIntro.phone.prefix}-${this.resumeIntro.phone.lineNumber}`
+    const phone = this.resumeIntro.phone
+    this.resumeIntro.phoneString = `${phone.countryCode}-${phone.areaCode}-${phone.prefix}-${phone.lineNumber}`
   }
 }
