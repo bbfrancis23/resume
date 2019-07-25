5eedf53752bb657cc954d8b39ac155c27c57680a
Brian <bbfrancis23@gmail.com>
2019-07-25 15:48:51 -0700

    Add server

--- src/app/page/page-items/resume-intro/resume-intro.component.ts
+++ src/app/page/page-items/resume-intro/resume-intro.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core'
 import { Unsubscriber } from '../../../unsubscriber/unsubscriber'
 
-
-
 export interface PhoneNumber {
   countryCode: number
   areaCode: number
@@ -21,7 +19,6 @@ export interface ResumeIntro {
 }
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'resume-intro',
   templateUrl: 'resume-intro.component.html'
 })
@@ -34,7 +31,7 @@ export class ResumeIntroComponent extends Unsubscriber {
     email: 'bbfrancis23@gmail.com',
     phone: { countryCode: 1, areaCode: 801, prefix: 867, lineNumber: 1885 },
     phoneString: '',
-    // tslint:disable-next-line:max-line-length
+    // tslint:disable-next-line:max-line-length until this gets put into database
     text: 'I have had over 15 year experience in Web Development and Programming. I studied Computer Science at Weber State University. I was responsible for the “Marine Classifieds” program at iboats.com. This program collects, edits, displays marine advertisements (mainly boats for sale) on boats.iboats.com, from boat dealers, brokers and individual users. This included “For Sale By Owner”, “New Boat research”, “Dealer Locator” and various backend administration programs. During my tenure at iboats classifieds ads went from a few thousand to nearly fifty thousand. I have worked various freelance job. I worked at Arizona Department of Environmental Quality at first as a Contractor and then was hired as an Angular Specialist.'
   }
 
